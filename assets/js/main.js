e9c2e96085243a1395f731cbc568252ef85c25eb
Batubo Victory <victorybatubo76@gmail.com>
2025-07-08 18:32:46 +0100

    Add HTML content and declare DOM variables

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const location = document.querySelector('#location');
+const loadingMsg = document.querySelector('#loadingMsg');
+const showWeatherBtn = document.querySelector('#showWeatherBtn');
+const temperature = document.querySelector('#temperature');
+const feeling = document.querySelector('#feeling');
+const weatherDescription = document.querySelector('#weatherDescription');
+const city = document.querySelector('#city');
+
